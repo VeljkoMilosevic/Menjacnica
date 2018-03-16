a367135e96ed9ea60f08f5aa0e45dcb8b2ccf165
Veljko <veljkoveljko@hotmail.com>
2018-03-16 19:02:16 +0100

    Kreirana klasa implementacije intefejsa

---
+++ Menjacnica/src/interfejsmenjanica/Implemntacija.java
@@ -0,0 +1,25 @@
+package interfejsmenjanica;
+
+import menjacnica.Valuta;
+
+public class Implemntacija implements Interfejs {
+
+	@Override
+	public void dodadavanjeKursa(Valuta m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void brisanjeKursa(Valuta m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Valuta pronalazanjeKursa(String naziv) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
